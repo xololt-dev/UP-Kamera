9243185f2ffdb68d07512e6f85ab6fe8329ad86e
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-11-19 19:11:28 +0100

    Before lab preparation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -21,6 +21,8 @@ using Windows.Media.Core;
 using Windows.Media.Effects;
 using Windows.Media.MediaProperties;
 using Windows.Storage.FileProperties;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
@@ -34,7 +36,7 @@ namespace UP
     {
         MediaCapture mediaCapture;
         LowLagMediaRecording _mediaRecording;
-        bool isPreviewing;
+        bool isPreviewing = true;
 
         public MainPage()
         {
@@ -45,12 +47,53 @@ namespace UP
         private async void initMediaCapture()
         {
             mediaCapture = new MediaCapture();
+            string videoDeviceId = await GetVideoProfileSupportedDeviceIdAsync(Windows.Devices.Enumeration.Panel.Back);
+
+            if (string.IsNullOrEmpty(videoDeviceId))
+            {
+                // No devices on the specified panel support video profiles. .
+                System.Diagnostics.Debug.WriteLine("isnullorempty");
+            }
+
             MediaCaptureInitializationSettings settings = new MediaCaptureInitializationSettings();
-            await mediaCapture.InitializeAsync(new MediaCaptureInitializationSettings
+            settings.VideoDeviceId = videoDeviceId;
+            settings.StreamingCaptureMode = StreamingCaptureMode.Video;
+            await mediaCapture.InitializeAsync(settings);
+
+            PopulateStreamPropertiesUI(MediaStreamType.VideoRecord, comboBoxFPS);
+                /*new MediaCaptureInitializationSettings
             {
                 StreamingCaptureMode = StreamingCaptureMode.Video
-            });
+            });*/
             // mediaCapture.Failed += MediaCapture_Failed;
+        }
+
+        public async Task<string> GetVideoProfileSupportedDeviceIdAsync(Windows.Devices.Enumeration.Panel panel)
+        {
+            string deviceId = string.Empty;
+
+            // Finds all video capture devices
+            DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            List<string> items = new List<string>();
+
+            foreach (var device in devices)
+            {
+                items.Add(device.Name);
+                // deviceId = device.Id;
+                // Check if the device on the requested panel supports Video Profile
+                
+                if (MediaCapture.IsVideoProfileSupported(device.Id) )//&& device.EnclosureLocation.Panel == panel)
+                {
+                    // We've located a device that supports Video Profiles on expected panel
+                    deviceId = device.Id;    
+                    // break;
+                }
+            }
+
+            cameraList.ItemsSource = items;
+            
+            return deviceId;
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
@@ -118,8 +161,8 @@ namespace UP
                 return;
             }
 
-            mediaPlayerElement.Source = MediaSource.CreateFromStorageFile(videoFile);
-            mediaPlayerElement.MediaPlayer.Play();
+            //mediaPlayerElement.Source = MediaSource.CreateFromStorageFile(videoFile);
+            //mediaPlayerElement.MediaPlayer.Play();
 
             StorageFolder destinationFolder =
                 await ApplicationData.Current.LocalFolder.CreateFolderAsync("ProfilePhotoFolder",
@@ -131,7 +174,7 @@ namespace UP
         {
             // Prepare and capture photo
             var lowLagCapture = await mediaCapture.PrepareLowLagPhotoCaptureAsync(ImageEncodingProperties.CreateUncompressed(MediaPixelFormat.Bgra8));
-
+            
             var capturedPhoto = await lowLagCapture.CaptureAsync();
             var softwareBitmap = capturedPhoto.Frame.SoftwareBitmap;
 
@@ -188,5 +231,143 @@ namespace UP
             await _mediaRecording.StopAsync();
             await _mediaRecording.FinishAsync();
         }
+
+        private void CheckIfStreamsAreIdentical()
+        {
+            if (mediaCapture.MediaCaptureSettings.VideoDeviceCharacteristic == VideoDeviceCharacteristic.AllStreamsIdentical ||
+                mediaCapture.MediaCaptureSettings.VideoDeviceCharacteristic == VideoDeviceCharacteristic.PreviewRecordStreamsIdentical)
+            {
+                System.Diagnostics.Debug.WriteLine("Preview and video streams for this device are identical. Changing one will affect the other");
+            }
+        }
+
+        private void PopulateStreamPropertiesUI(MediaStreamType streamType, ComboBox comboBox, bool showFrameRate = true)
+        {
+            // Query all properties of the specified stream type 
+            IEnumerable<StreamPropertiesHelper> allStreamProperties =
+                mediaCapture.VideoDeviceController.GetAvailableMediaStreamProperties(streamType).Select(x => new StreamPropertiesHelper(x));
+
+            // Order them by resolution then frame rate
+            allStreamProperties = allStreamProperties.OrderByDescending(x => x.Height * x.Width).ThenByDescending(x => x.FrameRate);
+
+            // Populate the combo box with the entries
+            foreach (var property in allStreamProperties)
+            {
+                ComboBoxItem comboBoxItem = new ComboBoxItem();
+                comboBoxItem.Content = property.GetFriendlyName(showFrameRate);
+                comboBoxItem.Tag = property;
+                comboBox.Items.Add(comboBoxItem);
+            }
+        }
+
+        private async void VideoSettings_Changed(object sender, RoutedEventArgs e)
+        {
+            if (isPreviewing)
+            {
+                var selectedItem = (sender as ComboBox).SelectedItem as ComboBoxItem;
+                var encodingProperties = (selectedItem.Tag as StreamPropertiesHelper).EncodingProperties;
+                await mediaCapture.VideoDeviceController.SetMediaStreamPropertiesAsync(MediaStreamType.VideoRecord, encodingProperties);
+            }
+        }
+    }
+
+    class StreamPropertiesHelper
+    {
+        private IMediaEncodingProperties _properties;
+
+        public StreamPropertiesHelper(IMediaEncodingProperties properties)
+        {
+            if (properties == null)
+            {
+                throw new ArgumentNullException(nameof(properties));
+            }
+
+            // This helper class only uses VideoEncodingProperties or VideoEncodingProperties
+            if (!(properties is ImageEncodingProperties) && !(properties is VideoEncodingProperties))
+            {
+                throw new ArgumentException("Argument is of the wrong type. Required: " + typeof(ImageEncodingProperties).Name
+                    + " or " + typeof(VideoEncodingProperties).Name + ".", nameof(properties));
+            }
+
+            // Store the actual instance of the IMediaEncodingProperties for setting them later
+            _properties = properties;
+        }
+
+        public uint Width
+        {
+            get
+            {
+                if (_properties is ImageEncodingProperties)
+                {
+                    return (_properties as ImageEncodingProperties).Width;
+                }
+                else if (_properties is VideoEncodingProperties)
+                {
+                    return (_properties as VideoEncodingProperties).Width;
+                }
+
+                return 0;
+            }
+        }
+
+        public uint Height
+        {
+            get
+            {
+                if (_properties is ImageEncodingProperties)
+                {
+                    return (_properties as ImageEncodingProperties).Height;
+                }
+                else if (_properties is VideoEncodingProperties)
+                {
+                    return (_properties as VideoEncodingProperties).Height;
+                }
+
+                return 0;
+            }
+        }
+
+        public uint FrameRate
+        {
+            get
+            {
+                if (_properties is VideoEncodingProperties)
+                {
+                    if ((_properties as VideoEncodingProperties).FrameRate.Denominator != 0)
+                    {
+                        return (_properties as VideoEncodingProperties).FrameRate.Numerator /
+                            (_properties as VideoEncodingProperties).FrameRate.Denominator;
+                    }
+                }
+
+                return 0;
+            }
+        }
+
+        public double AspectRatio
+        {
+            get { return Math.Round((Height != 0) ? (Width / (double)Height) : double.NaN, 2); }
+        }
+
+        public IMediaEncodingProperties EncodingProperties
+        {
+            get { return _properties; }
+        }
+
+        public string GetFriendlyName(bool showFrameRate = true)
+        {
+            if (_properties is ImageEncodingProperties ||
+                !showFrameRate)
+            {
+                return Width + "x" + Height + " [" + AspectRatio + "] " + _properties.Subtype;
+            }
+            else if (_properties is VideoEncodingProperties)
+            {
+                return Width + "x" + Height + " [" + AspectRatio + "] " + FrameRate + "FPS " + _properties.Subtype;
+            }
+
+            return String.Empty;
+        }
+
     }
 }
